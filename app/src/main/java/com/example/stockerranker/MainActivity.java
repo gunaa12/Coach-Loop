c702cc3f234090a86c5360da306487505b5a8961
Jon-Tokad <jtokad@purdue.edu>
2022-01-22 11:00:01 -0500

    Add startscreen and login UI

--- app/src/main/java/com/example/stockerranker/MainActivity.java
+++ app/src/main/java/com/example/stockerranker/MainActivity.java
@@ -5,7 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
@@ -20,5 +23,14 @@ public class MainActivity extends AppCompatActivity {
         System.out.println("helloooooooooooo");
         mAuth = FirebaseAuth.getInstance();
         mAuth.createUserWithEmailAndPassword("testemail@gmail.com", "testpass").addOnCompleteListener(task -> System.out.println("finished"));
+
+        Button btn = (Button)findViewById(R.id.btnLogin);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            }
+        });
     }
 }
