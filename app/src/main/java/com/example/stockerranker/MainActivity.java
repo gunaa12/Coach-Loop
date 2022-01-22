270ede6917baab609c10c5b13ca8887dd7236d5c
Guna <gunavikasavula@gmail.com>
2022-01-22 02:07:28 -0500

    authentication working

--- app/src/main/java/com/example/stockerranker/MainActivity.java
+++ app/src/main/java/com/example/stockerranker/MainActivity.java
@@ -2,14 +2,23 @@ package com.example.stockerranker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
 import android.os.Bundle;
 
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        System.out.println("helloooooooooooo");
+        mAuth = FirebaseAuth.getInstance();
+        mAuth.createUserWithEmailAndPassword("testemail@gmail.com", "testpass").addOnCompleteListener(task -> System.out.println("finished"));
     }
 }
