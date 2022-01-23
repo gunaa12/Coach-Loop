35cacb3c66f040dfa1af0f2c595f47658ac287c1
Guna <gunavikasavula@gmail.com>
2022-01-22 22:23:25 -0500

    refined beginning ui

--- app/src/main/java/com/example/stockerranker/HomeActivity.java
+++ app/src/main/java/com/example/stockerranker/HomeActivity.java
@@ -8,17 +8,29 @@ import android.view.View;
 import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.HashMap;
 import java.util.UUID;
 
 public class HomeActivity extends AppCompatActivity {
 
-
     private FirebaseAuth mAuth;
-
+    private FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        db = FirebaseFirestore.getInstance();
+        Map<String, Object> coach = new HashMap<String, Object>();
+        coach.put("name", "test");
+        coach.put("school", "test");
+        coach.put("wins", 3);
+        coach.put("losses", 2);
+        coach.put("rating", 1);
+        coach.put("review", "test");
+        coach.put("avgPointWin", 1.1);
+        db.collection("Coaches").add(coach);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
