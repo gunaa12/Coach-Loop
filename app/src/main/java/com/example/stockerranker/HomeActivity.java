dd1934fd64ce37d90718ca2fe1d487e67a6e0a5c
Guna <gunavikasavula@gmail.com>
2022-01-22 22:36:32 -0500

    released upload stress with ml training files

--- app/src/main/java/com/example/stockerranker/HomeActivity.java
+++ app/src/main/java/com/example/stockerranker/HomeActivity.java
@@ -6,30 +6,37 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
-import java.util.UUID;
+import java.util.Map;
 
 public class HomeActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     private FirebaseFirestore db;
 
+    private void pushToCloud(String name, String school, String review, int wins, int losses, int rating, float avgPointWin) {
+        try {
+            Map<String, Object> coach = new HashMap<String, Object>();
+            coach.put("school", school);
+            coach.put("wins", wins);
+            coach.put("losses", losses);
+            coach.put("rating", rating);
+            coach.put("review", review);
+            coach.put("avgPointWin", avgPointWin);
+            db.collection("Coaches").document(name).set(coach);
+        } catch (Exception e) {
+            Toast.makeText(HomeActivity.this, "Push to Cloud Failed!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         db = FirebaseFirestore.getInstance();
-        Map<String, Object> coach = new HashMap<String, Object>();
-        coach.put("name", "test");
-        coach.put("school", "test");
-        coach.put("wins", 3);
-        coach.put("losses", 2);
-        coach.put("rating", 1);
-        coach.put("review", "test");
-        coach.put("avgPointWin", 1.1);
-        db.collection("Coaches").add(coach);
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
