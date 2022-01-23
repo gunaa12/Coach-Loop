154da8a2b95092bb6bfc73ba922264b4b2305374
Guna <gunavikasavula@gmail.com>
2022-01-22 23:18:52 -0500

    idk

--- app/src/main/java/com/example/stockerranker/HomeActivity.java
+++ app/src/main/java/com/example/stockerranker/HomeActivity.java
@@ -11,6 +11,10 @@ import android.widget.Toast;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -32,6 +36,15 @@ public class HomeActivity extends AppCompatActivity {
         } catch (Exception e) {
             Toast.makeText(HomeActivity.this, "Push to Cloud Failed!", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    private int getMLRating(String review) throws IOException {
+        File file = new File("../../../../../ml/model2/rating_com_file.txt");
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        String line = br.readLine();
+        System.out.println(line);
+        br.close();
+        return 0;
     }
 
     @Override
